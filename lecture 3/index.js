02884ab17845dba1ab28f4b37b5c26995e8956ae
Andrew Nagy <andrew.n@trufla.com>
2021-12-29 11:01:01 +0200

    lecture 3b

--- lecture 3/index.js
+++ lecture 3/index.js
@@ -1,32 +1,25 @@
 const mongoose = require("mongoose");
 const express = require("express");
-const User = require("./models/user");
+const userRouter = require("./Routes/user");
+const { logger, auth } = require("./middleware");
 
 const app = express();
 
+//Middleware
 app.use(express.json());
+app.use(logger);
+// app.use(auth);
+app.use("/users", userRouter);
 
 mongoose
     .connect("mongodb://localhost:27017/mydb")
-    .then(() => console.log("successfully connected with the database"))
+    .then(() => {
+        app.listen(3000, () => {
+            console.log("server running on port 3000");
+        });
+        console.log("successfully connected with the database")
+    })
     .catch(() => {
         console.log("error connecting to mongodb");
     });
 
-app.get("/users", async (req, res) => {
-    const users = await User.find();
-    res.json(users);
-});
-
-
-app.post("/users", async (req, res) => {
-    const { age, name } = req.body;
-    const newUser = new User({ name, age });
-    await newUser.save();
-    res.json(newUser);
-});
-
-
-app.listen(3000, () => {
-    console.log("server running on port 3000");
-});
